7fd642364315a2bd233db4db1d415869a2124b32
navodii <navodididilshara@gmail.com>
2024-10-22 14:47:12 +0530

    Customer form navigation button

--- src/main/java/controller/item/ItemFormController.java
+++ src/main/java/controller/item/ItemFormController.java
@@ -6,14 +6,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Item;
 import util.CrudUtil;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
@@ -176,4 +180,13 @@ public class ItemFormController implements Initializable {
         tblItems.setItems(itemObservableList);
     }
 
+    public void btnCustomerFormOnAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/customer_form.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
